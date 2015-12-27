7e645ac3a854a0b0ae4ea52097074cfb5fcf3316
pikesley <github@orgraphone.org>
2015-12-27 23:46:20 +0000

    I think I can SASS now

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-assetpack'
 gem 'thin'
 gem 'rake'
+gem 'sass'
+gem 'tilt', '~> 1'
 
 group :test do
   gem 'cucumber'
--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -1,6 +1,26 @@
 require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'tilt'
 
 class Dashboard < Sinatra::Base
+  register Sinatra::AssetPack
+
+  assets do
+    js :application, [
+      '/js/dashboard.js'
+    ]
+
+    css :application, [
+      '/css/jqueryui.css',
+      '/css/reset.css',
+      '/css/foundation.css',
+      '/css/app.css'
+    ]
+
+    js_compression :jsmin
+    css_compression :sass
+  end
+
   get '/' do
     @content = '<h1>Home Dashboard</h1>'
     @title = 'Dashboard'
