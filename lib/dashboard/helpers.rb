00acb1b927601379135da094da929279b046754d
pikesley <github@orgraphone.org>
2015-12-28 19:39:03 +0000

    Redundant for now

--- lib/dashboard/helpers.rb
+++ lib/dashboard/helpers.rb
@@ -1,6 +1,1 @@
-module Dashboard
-  def self.date_fixer date
-    require "pry" ; binding.pry
-    '3rd December'
-  end
-end
+
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,7 +1,1 @@
-module Dashboard
-  describe 'date fixer' do
-    it 'parses a date' do
-      expect(Dashboard.date_fixer '2015-12-03').to eq '3rd December'
-    end
-  end
-end
+
